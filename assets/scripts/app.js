88341fd6b3117f135d4facd4a2a4231a0e4dd03e
Treasure Kabareebe <treasurecalm@gmail.com>
2020-06-17 23:33:51 +0300

    Update button label on moving project items

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,25 +15,26 @@ class DOMHelper {
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id, updateProjectListsFunction){
+  constructor(id, updateProjectListsFunction, type){
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
-    this.connectSwitchButton();
+    this.connectSwitchButton(type);
   }
 
   connectMoreInfoButton(){}
 
-  connectSwitchButton(){
+  connectSwitchButton(type){
     const projectItemElement = document.getElementById(this.id);
     let switchBtn = projectItemElement.querySelector('button:last-of-type');
     switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
     switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id));
   }
 
   update(updateProjectListsFn, type){
     this.updateProjectListsHandler = updateProjectListsFn;
-    this.connectSwitchButton()
+    this.connectSwitchButton(type)
   }
 }
 
@@ -44,7 +45,7 @@ class ProjectList {
     this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for(const prjItem of prjItems){
-      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
+      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type));
     }
     console.log(this.projects);
   }

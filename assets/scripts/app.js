144ff43bc047cb1fa8a17676ce4f06b86e673ba1
Treasure Kabareebe <treasurecalm@gmail.com>
2020-06-17 23:15:55 +0300

    Add logic for finnish button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,27 @@
+class DOMHelper {
+  static moveElement(elementId, newDestinationSelector){
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
+}
+
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id){
+  constructor(id, updateProjectListsFunction){
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton(){}
+
+  connectSwitchButton(){
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id));
   }
 }
 
@@ -10,11 +29,29 @@ class ProjectList {
   projects = [];
 
   constructor(type){
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for(const prjItem of prjItems){
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
     }
     console.log(this.projects);
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction){
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project){
+    console.log(project);
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`)
+  }
+
+  switchProject(projectId){
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.switchHandler(this.projects.find(p => p.id === projectId));
+    this.projects = this.projects.filter(p => p.id !== projectId);
   }
 }
 
@@ -22,6 +59,12 @@ class App {
   static init(){
     const activeProjectList = new ProjectList('active');
     const finishedProjectList = new ProjectList('finished');
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
   }
 }
 
